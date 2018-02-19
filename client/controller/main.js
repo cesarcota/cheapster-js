95b2034387848805461db15b42c0f57875938f1c
Luis Serpa <luis.ff.serpa@hotmail.com>
2018-02-19 16:41:11 -0100

    Email authentication on the register side

--- client/controller/main.js
+++ client/controller/main.js
@@ -1,71 +1,65 @@
-
-
 if(Meteor.isClient){
 
-  Template.register.events({
-    "submit form": function(event){
-      event.preventDefault();
-      var emailVar = event.target.registerEmail.value;
-      var passwordVar = event.target.registerPassword.value;
+ Template.register.events({
+   "submit form": function(event){
+     event.preventDefault();
+     var emailVar = event.target.registerEmail.value;
+     var passwordVar = event.target.registerPassword.value;
+     var repeatPassword = event.target.repeatPassword.value;
 
-      //Can't have empty fields
-      if(!emailVar || !passwordVar){
-        FlashMessages.sendError("All fields must be filled.");
-        throw new Meteor.Error("All fields must be filled.");
-      }
+     //Both passwords must be equal
+     if(repeatPassword !== passwordVar){
+       FlashMessages.sendError("Passwords must be equal");
+       throw new Meteor.Error("Passwords must be equal");
+     }
 
-      //Verify if the user already exists
-      if (Meteor.users.find(emailVar)) {
-        FlashMessages.sendError("A user with email " + emailVar + " already exists!");
-        throw new Meteor.Error("A user with email " + emailVar + " already exists!");
-      }
+     //Create the user
+     Accounts.createUser({
+       email: emailVar,
+       password: passwordVar
+     }, function(error) {
+       if (error) {
+         FlashMessages.sendError(error.reason);
+         throw new Meteor.Error(error.reason)
+       } else {
+           Router.go('/');
+       }
+   });
+     console.log("Form submitted.");
 
-      //Create the user
-      Accounts.createUser({
-        email: emailVar,
-        password: passwordVar
-      });
-
-      console.log("Form submitted.");
-    }
-
-  });
-
+ }
 
 
-  Template.login.events({
-    "submit form": function(event){
-      event.preventDefault();
-      var emailVar = event.target.loginEmail.value;
-      var passwordVar = event.target.loginPassword.value;
+ });
 
-      //Can't have empty fields
-      if(!emailVar || !passwordVar){
-        FlashMessages.sendError("All fields must be filled.");
-        throw new Meteor.Error("All fields must be filled.");
-      }
 
-      //Perform the login action with a Meteor collection
-      Meteor.loginWithPassword(emailVar, passwordVar, function(error){
-        if(Meteor.user){
-          FlashMessages.sendSuccess("Log in success");
-        }else{
-          FlashMessages.sendError("Failled to find email or password.");
-          throw new Meteor.Error("Failled to find email or password.");
-        }
-      });
 
-      console.log("Form submitted.");
-    }
-  });
+ Template.login.events({
+   "submit form": function(event){
+     event.preventDefault();
+     var emailVar = event.target.loginEmail.value;
+     var passwordVar = event.target.loginPassword.value;
+
+     //Can't have empty fields
+     if(!emailVar || !passwordVar){
+       FlashMessages.sendError("All fields must be filled.");
+       throw new Meteor.Error("All fields must be filled.");
+     }
+
+     //Perform the login action with a Meteor collection
+     Meteor.loginWithPassword(emailVar, passwordVar);
+
+     console.log("Form submitted.");
+   }
+ });
 /*
 
-  Template.dashboard.events({
-    "click .logout": function (event){
-      event.preventDefault();
-      Meteor.logout();
-    }
-  });
+ Template.dashboard.events({
+   "click .logout": function (event){
+     event.preventDefault();
+     Meteor.logout();
+   }
+ });
 
 */
 }
