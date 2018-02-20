c69b33f57c87528cfe814e715bd0a380ed79b9d0
Diogo Brandão <diogobr2@hotmail.com>
2018-02-20 15:27:40 -0100

    css ta pretty

--- client/controller/addgroup.js
+++ client/controller/addgroup.js
@@ -1,9 +1,16 @@
+Items = new Meteor.Collection('items');
 
 
-export const Groups = new Mongo.Collection('groups');
 
-function addGroup(){
-    
 
-    return false;
-}
+//if(Meteor.isClient) { //if its on client side
+
+Template.addgroup.events({
+  'click .submit': function() { //faz o evento acontecer
+      Items.insert({
+        item: $('.the_item').val()
+      })
+    }
+});
+
+//}
