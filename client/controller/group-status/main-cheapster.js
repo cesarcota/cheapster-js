6711bf44bcfd09cdf5f9f3e14e16d7fcaa6b3b72
Luis Serpa <luis.ff.serpa@hotmail.com>
2018-03-26 16:01:32 +0000

    fix the main cheapster calc

--- client/controller/group-status/main-cheapster.js
+++ client/controller/group-status/main-cheapster.js
@@ -5,10 +5,14 @@ Template.mainCheapster.helpers({
         //Now it needs to select the person with the lowest ratio accuracy
         var tempCheapster = 1;
         var mainCheapster;
+        console.log("GROUP USERS: ", groupUsers);
         groupUsers.forEach(function(userAccuracy) {
+            console.log("USER ACCURACY: ", userAccuracy);
+            console.log("TEMP CHEAPSTER: ", tempCheapster);
             if (userAccuracy.accuracy <= tempCheapster) {
                 mainCheapster = userAccuracy.displayName;
-                tempCheapster = mainCheapster;
+                console.log("MAIN CHEAPSTER: ", mainCheapster);
+                tempCheapster = userAccuracy.accuracy;
             }
         });
         return mainCheapster;
