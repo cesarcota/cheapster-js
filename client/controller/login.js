b5d6e172522e522615e49db4ec0117846bb704f0
Luis Serpa <luis.ff.serpa@gmail.com>
2018-04-11 19:59:35 +0000

    login controller

--- client/controller/login.js
+++ client/controller/login.js
@@ -1,30 +1,43 @@
 Template.login.events({
-    "submit form": function (event) {
+    "submit form": function(event) {
         event.preventDefault();
         var emailVar = event.target.loginEmail.value;
         var passwordVar = event.target.loginPassword.value;
 
-        Meteor.loginWithPassword({email: emailVar}, passwordVar, function (err, success) {
+        //Convert the password to hashcode
+        const hash = require("js-hash-code");
+        passwordVar = hash(passwordVar);
 
-            console.log("called auth.. got", arguments)
-            if (!success) {
+        //Authentication
+        Meteor.call("findByEmail", emailVar, function(error, tempUser) {
+            if (error) {
                 FlashMessages.sendError("Not a valid email or password.");
                 return;
+            } else {
+                if (tempUser !== undefined) {
+                    //This means that it entered here to do the Authentication
+                    //Checks the password of the user
+                    if (tempUser.password !== passwordVar) {
+                        FlashMessages.sendError(
+                            "Not a valid email or password."
+                        );
+                        return;
+                    } else {
+                        Session.set("sessionUser", tempUser);
+                        //Store the user in a cookie
+                        document.cookie = tempUser.email;
+                        //document.cookie =
+                        name +
+                            "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=[username=luis@hotmail.com];";
+
+                        //document.cookie = undefined;
+                        Router.go("/dashboard");
+                    }
+                } else {
+                    FlashMessages.sendError("Not a valid email or password.");
+                    return;
+                }
             }
-/*
-            Session.set("sessionUser", tempUser);
-            //Store the user in a cookie
-            document.cookie = tempUser.email;
-            //document.cookie =
-            name +
-                "=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=[username=luis@hotmail.com];";
-
-            //document.cookie = undefined;
-            Router.go("/dashboard");
-            */
-
-
-
         });
     }
 });
